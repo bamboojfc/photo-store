ca1bdd8557e20c931a7af4dd1b4ead06c21eeba3
Nannapas <bamboojfc@gmail.com>
2017-12-15 15:03:00 +0700

    add log file

--- PhotoStore.py
+++ PhotoStore.py
@@ -11,6 +11,26 @@ Created on Thu Dec 14 12:27:04 2017
 import os
 import json
 import re
+from shutil import copyfile
+
+class Log:
+
+    def __init__(self):
+        with open('./config.json', 'r') as f:
+            self.__config = json.load(f) 
+        self.__input_dir = self.__config['path']['input_dir']
+        self.__log_file = os.path.join(self.__input_dir, self.__config['path']['log_file'])
+
+    def write(self, text):
+        with open(self.__log_file, "a") as text_file:
+            print text
+            text_file.write(text + '\r\n')
+
+    def end(self):
+        text = '============== DONE ==============='
+        with open(self.__log_file, "a") as text_file:
+            print text
+            text_file.write(text + '\r\n')
 
 class PhotoStore:
 
@@ -26,14 +46,16 @@ class PhotoStore:
             self.__filter_in_regexp = '||'.join(self.__filter_in)
         if len(self.__filter_out) > 0:
             self.__filter_out_regexp = '||'.join(self.__filter_out)
+        self.__log = Log()
         # except Exception:
-        #     print 'ERROR : no config file.'
+        #     self.__log.write('ERROR : no config file.')
         #     exit()
         
     def list_dir(self):
         input_dir_list = os.listdir(self.__input_dir)  
-        print 'Get all directories from', self.__input_dir
-        print 'All:', input_dir_list
+        self.__log.write('Get all directories from ' + str(self.__input_dir))
+        self.__log.write('All: ' + str(', '.join(input_dir_list)))
+        
         res = []
         if len(self.__filter_in) > 0:
             # it gets only directories matching filter_in regex list
@@ -53,7 +75,7 @@ class PhotoStore:
                         res.append(f)
                 except:
                     pass
-        print 'Filtered:', res
+        self.__log.write('Filtered: '+ ', '.join(res))
         return res
 
     def walk_in_directory(self, current_input_path = None, d = None, current_output_path = None):
@@ -65,11 +87,9 @@ class PhotoStore:
         current_output_path = os.path.join(current_output_path, os.path.basename(d))
         
         if os.path.isdir(current_input_path):
-            print 'walk into directory: ', current_input_path
             self.mkdir(current_output_path)            
             file_list = os.listdir(current_input_path)
-
-            print 'Found', len(file_list), 'directories & files.', file_list                        
+                      
             for f in file_list:
                 self.walk_in_directory(current_input_path, f, current_output_path)
         else:
@@ -77,23 +97,25 @@ class PhotoStore:
                 self.copy_file(current_input_path, current_output_path)
 
     def mkdir(self, outdir):
-        print 'Create directory:', outdir
+        self.__log.write('Create directory: ' + str(outdir))
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         else:
-            print 'FAILED:', outdir, 'has been already created.'
+            self.__log.write('\tFAILED: ' + str(outdir) + ' has been already created.')
 
     def copy_file(self, infile, outfile):
-        print 'Copy file from', infile, 'to', outfile
+        self.__log.write('\tCopy file from ' + str(infile) + ' to ' + str(outfile))
+        copyfile(infile, outfile)
 
     def start(self):
         self.__directories = self.list_dir()
-        print 'list of all directories:', self.__directories, '\n'
+        self.__log.write('list of all directories:' + str(self.__directories) + '\n')
         for d in self.__directories:
-            print '#### Start walking into directory: ', d, '####'
+            self.__log.write('#### Start walking into directory: ' + str(d) + '####')
             self.mkdir(self.__output_dir)          
             self.walk_in_directory(d = d)
-            print '\n'
+            self.__log.write('\n')
+        self.__log.end()
 
 photoStore = PhotoStore()
 photoStore.start()
